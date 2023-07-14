c384f5e8c7351276c49e59363b20543986babf7f
999Thando <zukula99@gmail.com>
2023-07-13 20:51:34 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write("Size of a char: %d byte(s)\n", size of (char));
+	write("Size of an int: %d byte(s)\n", size of (int));
+	write("Size of a long int: %d byte(s)\n", size of (long int));
+	write("Size of a float: %d byte(s)\n", size of (float));
+	return (0);
+}
