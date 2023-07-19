fc7390265052e729859f69c22a05cc3b8795dd66
999Thando <zukula99@gmail.com>
2023-07-18 18:57:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -7,9 +7,7 @@
  */
 
 int main(void)
-
 {
-
     times_table();
     return (0);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,9 @@ void times_table(void)
 	int u;
 	int d;
 
-	for (x = '0'; x <= '9'; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	for (y = '0'; y <= '9'; y++)
+	for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
 	if (z > 9)
