42f4e22ac5081cfa2696471796e1e7e9cdfe3901
999Thando <zukula99@gmail.com>
2023-07-18 17:02:02 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Checks for alphabetic character
- * @C: The character to be checked
+ * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
@@ -14,5 +14,5 @@ int _isalpha(int c)
 	{
 	return (1);
 	}
-	return (0);	
+	return (0);
 }
