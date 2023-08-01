335ead1e40424514523ff1420dca5e59ba3d0ccc
999Thando <zukula99@gmail.com>
2023-08-01 16:13:38 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: integer to be checked
+ * Return: 1 if c is upper, 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	return (1);
+	}
+	return (0);
+	
+}
